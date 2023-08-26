bluez/bluez

commit fde32ff9c9c0ab531767c4539cd0b5da3aec289b

shared/gatt-client: Allow registering with NULL callback
This makes bt_gatt_client_register_notify allow registering with NULL
callback which is interpreted as the CCC write has already been
performed therefore it won't be written again.

src/shared/gatt-client.c
@@ -1711,8 +1711,11 @@ static unsigned int register_notify(struct bt_gatt_client *client,
 		 * descriptor.
 		 */
 		chrc = notify_chrc_create(client, handle);
-		if (!chrc)
+		if (!chrc) {
+			DBG(client, "Unable to locate characteristic at 0x%04x",
+							handle);
 			return 0;
+		}
 	}
 
 	/* Fail if we've hit the maximum allowed notify sessions */
@@ -1750,9 +1753,10 @@ static unsigned int register_notify(struct bt_gatt_client *client,
 	}
 
 	/*
-	 * If the ref count > 1, then notifications are already enabled.
+	 * If the ref count > 1, ccc handle cannot be found or registration
+	 * callback is not set consider notifications are already enabled.
 	 */
-	if (chrc->notify_count > 1 || !chrc->ccc_handle) {
+	if (chrc->notify_count > 1 || !chrc->ccc_handle || !callback) {
 		complete_notify_request(notify_data);
 		return notify_data->id;
 	}
@@ -2175,6 +2179,9 @@ static void notify_cb(struct bt_att_chan *chan, uint8_t opcode,
 {
 	struct bt_gatt_client *client = user_data;
 	struct value_data data;
+
+	if (queue_isempty(client->notify_list))
+		return;
 
 	bt_gatt_client_ref(client);
 
@@ -3670,7 +3677,8 @@ unsigned int bt_gatt_client_register_notify(struct bt_gatt_client *client,
 				void *user_data,
 				bt_gatt_client_destroy_func_t destroy)
 {
-	if (!client || !client->db || !chrc_value_handle || !callback)
+	if (!client || !client->db || !chrc_value_handle ||
+				(!callback && !notify))
 		return 0;
 
 	if (client->in_svc_chngd)

tedd-an/bluez-upstream-test

commit 25356744f9bfca932290544925552b2baf4bac6e

shared/gatt-client: always send ATT confirmations
I noticed after upgrading 5.66->5.68 that Bluez was no longer sending
confirmations (ATT opcode 0x1E) in response to indication opcodes (0x1D).

Commit fde32ff9c9c0 ("shared/gatt-client: Allow registering with NULL
callback") added an early return to the notify_cb function when the
current client's notify_list is empty. However, in this case, we will
also not send the confirmation back. This breaks protocol.

The devices I have generally respond to this by stopping
any new indications until ~15sec timeout or disconnection.

As far as I can see, when using D-Bus API all notify handlers are always
added on client clones, never on the 'root' client itself (the one
with !client->parent), so for the root client the notify_list is always
empty, making this issue very easy to trigger using D-Bus GATT API.

Ensure that we always send the confirmation, even if notify_list is empty.

Fixes: fde32ff9c9c0 ("shared/gatt-client: Allow registering with NULL callback")

src/shared/gatt-client.c
@@ -2230,45 +2230,46 @@ static void notify_cb(struct bt_att_chan *chan, uint8_t opcode,
 					void *user_data)
 {
 	struct bt_gatt_client *client = user_data;
-	struct value_data data;
-
-	if (queue_isempty(client->notify_list))
-		return;
 
 	bt_gatt_client_ref(client);
 
-	memset(&data, 0, sizeof(data));
+	if (!queue_isempty(client->notify_list)) {
+		struct value_data data;
 
-	if (opcode == BT_ATT_OP_HANDLE_NFY_MULT) {
-		while (length >= 4) {
+		memset(&data, 0, sizeof(data));
+
+		if (opcode == BT_ATT_OP_HANDLE_NFY_MULT) {
+			while (length >= 4) {
+				data.handle = get_le16(pdu);
+				length -= 2;
+				pdu += 2;
+
+				data.len = get_le16(pdu);
+				length -= 2;
+				pdu += 2;
+
+				if (data.len > length)
+					data.len = length;
+
+				data.data = pdu;
+
+				queue_foreach(client->notify_list,
+					      notify_handler, &data);
+
+				length -= data.len;
+				pdu += data.len;
+			}
+		} else {
 			data.handle = get_le16(pdu);
 			length -= 2;
 			pdu += 2;
 
-			data.len = get_le16(pdu);
-			length -= 2;
-			pdu += 2;
-
-			if (data.len > length)
-				data.len = length;
-
+			data.len = length;
 			data.data = pdu;
 
-			queue_foreach(client->notify_list, notify_handler,
-								&data);
-
-			length -= data.len;
-			pdu += data.len;
+			queue_foreach(client->notify_list,
+				      notify_handler, &data);
 		}
-	} else {
-		data.handle = get_le16(pdu);
-		length -= 2;
-		pdu += 2;
-
-		data.len = length;
-		data.data = pdu;
-
-		queue_foreach(client->notify_list, notify_handler, &data);
 	}
 
 	if (opcode == BT_ATT_OP_HANDLE_IND && !client->parent)