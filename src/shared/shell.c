7ba7170a4280544b3bd7b582240dd66ace65b0a6
Bastien Nocera <hadess@hadess.net>
2024-09-12 11:20:12 +0200

    shared/shell: Exit after printing the help
    Exit after handling --help, so as to avoid the daemon waiting to
    communicate with a D-Bus service that might not be running.

--- src/shared/shell.c
+++ src/shared/shell.c
@@ -1324,9 +1324,7 @@ void bt_shell_init(int argc, char **argv, const struct bt_shell_opt *opt)
 			return;
 		case 'h':
 			usage(argc, argv, opt);
-			data.argc = 1;
-			data.argv = &cmplt;
-			data.mode = 1;
+			exit(EXIT_SUCCESS);
 			goto done;
 		case 's':
 			if (optarg && data.init_fd < 0) {
