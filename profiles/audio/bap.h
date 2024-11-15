7516c3dbf7b30bbeea8a45865c5131cfcd0a436e
Iulia Tanasescu <iulia.tanasescu@nxp.com>
2024-11-15 12:38:23 +0200

    bap: Remove API to probe device from BASS
    This removes the BAP API to probe broadcasters from BASS, since BASS is
    now directly adding the Broadcast Audio Announcement Service UUID to the
    broadcaster device, and it will internally handle the probe.

--- profiles/audio/bap.h
+++ profiles/audio/bap.h
@@ -14,7 +14,6 @@ typedef void (*bap_stream_cb_t)(uint8_t bis, uint8_t sgrp,
 extern struct bt_iso_qos bap_sink_pa_qos;
 
 struct bt_bap *bap_get_session(struct btd_device *device);
-void bap_scan_delegator_probe(struct btd_device *device);
 
 bool parse_base(struct bt_iso_base *base, struct bt_iso_qos *qos,
 		util_debug_func_t func, bap_stream_cb_t handler,
